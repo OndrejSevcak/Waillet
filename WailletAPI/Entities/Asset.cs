867303956bd7d37b12a57d46c9a76f43acb4ee45
OndrejS <ondrejsevcak@gmail.com>
2026-02-22 23:19:06 +0100

    asset entity

---
+++ WailletAPI/Entities/Asset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WailletAPI.Entities;
+
+public partial class Asset
+{
+    public int Id { get; set; }
+
+    public string Symbol { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public byte Decimals { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public DateTimeOffset UpdatedAt { get; set; }
+}
